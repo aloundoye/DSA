53a5cdb560057135617cf70743f8e566a0703607
aloundoye <aloutndoye.an@gmail.com>
2025-03-14 21:38:03 +0000

    refactoring

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -12,8 +12,3 @@ def binary_search(arr, item):
         else:
             low = mid + 1
     return None
-
-my_list = [1,3,5,6,7,9]
-
-print(binary_search(my_list, 3))
-print(binary_search(my_list, 4))
