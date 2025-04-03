652df070f08c4d327b3a4b8eace595089110b57e
aloundoye <aloutndoye.an@gmail.com>
2025-04-03 19:39:39 +0000

    Update quicksort.py

--- algorithms/quicksort.py
+++ algorithms/quicksort.py
@@ -2,12 +2,17 @@ def quicksort(array):
     if len(array) < 2:
         return array
     else:
-        pivot = array[0]
-        less = [item for item in array[1:] if item <= pivot]
-        greater = [item for item in array[1:] if item > pivot]
+        mid = len(array) // 2  # Find the middle index
+        pivot = array[mid]  # Use the middle element as the pivot
+        
+        # Create the less and greater sublists (excluding the pivot)
+        less = [item for i, item in enumerate(array) if item <= pivot and i != mid]
+        greater = [item for i, item in enumerate(array) if item > pivot and i != mid]
+
         return quicksort(less) + [pivot] + quicksort(greater)
 
 
 
-print(quicksort([8,43,23,1,7,54,3,2,9,0,1]))
+# print(quicksort([8,43,23,1,7,54,3,2,9,0,1,8,43,23,1,7,54,3,2,9,0,1,]))
+print(quicksort([0, 0, 1, 1, 1, 1, 2, 2, 3, 3, 7, 7, 8, 8, 9, 9, 23, 23, 43, 43, 54, 54]))
             
